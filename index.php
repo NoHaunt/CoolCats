dd7815dfb008a527870e2f1feb3594560f0eeb10
NoFreya <kubsonr1@gmail.com>
2022-05-18 19:45:45 +0500

    best!

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="section-content">
-                            <h5 class="section-content-title">Самые самые популярные котики</h5>
+                            <h5 class="section-content-title">Самые самые котики!</h5>
                         </div>
                     </div>
                 </div>
