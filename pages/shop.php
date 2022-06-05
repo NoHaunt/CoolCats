841647fb5f9873ae358d15f27c220fbe11ba619d
NoFreya <kubsonr1@gmail.com>
2022-06-05 20:10:06 +0500

    header fix

--- pages/shop.php
+++ pages/shop.php
@@ -7,6 +7,17 @@ if (isset($_GET["Search"])) {
         header("Location: " . "shop.php");
     }
 }
+if(isset($_GET["toCart"])){
+    header("Location: " . "busket.php");
+}
+
+if (isset($_GET["ToLogin"])){
+    header("Location: " . "login.php");
+}
+
+if (isset($_GET["toProfile"])) {
+    header("Location: " . "profile.php");
+}
 
 
 $hostname = 'localhost';
